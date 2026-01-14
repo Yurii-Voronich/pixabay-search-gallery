9f4abe09cb278e29dd5ae44902a36a79efdacf00
Yurii <voronich97@gmail.com>
2026-01-14 12:44:37 +0200

    fix ghPages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
-
 export default defineConfig(({ command }) => {
   return {
+    base: command === 'serve' ? '/' : '/pixabay-gallery/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
